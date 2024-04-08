7cdec47635a3f59b1b086035aa9c03b3d8f36d4f
Edward Boguslavsky <eb.contactme@gmail.com>
2024-04-08 00:46:47 -0400

    Bot now functions with user IDs instead of usernames

--- index.js
+++ index.js
@@ -38,13 +38,13 @@ client.once('ready', () => {
 });
 
 // Check for birthdays and assign the birthday roles
-function checkBirthdays() {
+async function checkBirthdays() {
     // Get today's date and format into MM-DD format
     const today = new Date();
     const todayStr = (today.getMonth() + 1).toString().padStart(2, '0') + '-' + today.getDate().toString().padStart(2, '0');
 
     // Run for each server the bot is in
-    client.guilds.cache.forEach(guild => {
+    client.guilds.cache.forEach(async (guild) => {
         // Get the Birthday role
         const birthdayRole = guild.roles.cache.find(role => role.name === BIRTHDAY_ROLE_NAME);
 
@@ -62,17 +62,26 @@ function checkBirthdays() {
             // Retrieve user from ID
             const member = guild.members.cache.get(userId);
 
-            // Return error message if the user does not exist
-            if (member) {
-                // If it is the user's birthday and they don't have the birthday role, add it
-                if (isBirthday && !member.roles.cache.has(birthdayRole.id)) {
-                    member.roles.add(birthdayRole).then(() => console.log(`Added Birthday role to ${member.user.tag}`));
-                // If it is not the user's birthday and they have the birthday role, remove it
-                } else if (!isBirthday && member.roles.cache.has(birthdayRole.id)) {
-                    member.roles.remove(birthdayRole).then(() => console.log(`Removed Birthday role from ${member.user.tag}`));
+            // Add or remove the birthday role
+            try {
+                // Fetch the member using their user ID
+                const member = await guild.members.fetch(userId); 
+
+                if (isBirthday) {
+                    // If it is the user's birthday and they don't have the birthday role, add it
+                    if (!member.roles.cache.has(birthdayRole.id)) {
+                        member.roles.add(birthdayRole).then(() => console.log(`Added birthday role to ${member.user.tag}`));
+                        
+                    }
+                } else {
+                    // If it is not the user's birthday and they have the birthday role, remove it
+                    if (member.roles.cache.has(birthdayRole.id)) {
+                        member.roles.remove(birthdayRole).then(() => console.log(`Removed birthday role from ${member.user.tag}`));
+                    }
                 }
-            } else {
-                console.log(`The user "${userId}" does not exist in guild "${guild.name}"`);
+            } catch (error) {
+                // Return error message if the user does not exist
+                console.error(`Could not fetch member with ID ${userId}: ${error}`);
             }
         }
     });
